eac99fe9835a0dcac54a59af0e296d120e098a66
ai-tools-demo-user <shauki@juliatech.com>
2025-07-14 00:02:27 -0400

    Initial Commit

---
+++ Fibonacci.java
@@ -0,0 +1,9 @@
+// A naive, inefficient recursive function
+public class Fibonacci {
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
